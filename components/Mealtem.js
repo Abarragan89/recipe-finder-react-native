6680cd60f673f8d8b4f677afe3a474b888c1d1fc
Anthony Barragan <anthony.bar.89@gmail.com>
2023-12-21 07:54:59 -0800

    added mealitem

--- components/Mealtem.js
+++ components/Mealtem.js
@@ -1,4 +1,4 @@
-import { Text, View, Pressable, Image } from "react-native"
+import { Text, View, Pressable, Image, StyleSheet } from "react-native"
 
 function MealItem({ meal }) {
 
@@ -7,8 +7,8 @@ function MealItem({ meal }) {
             <Pressable>
                 <View>
                     {/* instead of requiring a local file, we set the uri with the url */}
-                    <Image source={{ uri: meal.imageUrl}} />
-                    <Text>{meal.title}</Text>
+                    <Image style={styles.image} source={{ uri: meal.imageUrl}} />
+                    <Text style={styles.title}>{meal.title}</Text>
                 </View>
             </Pressable>
         </View>
@@ -16,3 +16,15 @@ function MealItem({ meal }) {
 }
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 200
+    },
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18
+    }
+})
